1df19fa019776e5853c94108adc953e594709aca
losrobbos <losrobbos@web.de>
2021-05-07 22:11:05 +0200

    Added some sneaky indent...

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Test works" })
-})
+  app.get("/test", (req, res) => {
+    res.json({ message: "Test works" })
+  })
 
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!');
