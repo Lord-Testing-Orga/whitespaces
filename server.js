0b706b938e9bb8088117cfb5d275a465eb3adbd6
losrobbos <losrobbos@web.de>
2021-05-07 22:05:30 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Test works" })
+})
+
+app.listen(5000, () => {
+  console.log('Example app listening on port 5000!');
+});
+
+//Run app, then load http://localhost:5000 in a browser to see the output.
